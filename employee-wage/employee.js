577a1966e582f42af7913e5ded5608e93c95dde6
MANOJ-51 <m4anoj51@gmail.com>
2022-07-29 22:23:41 +0530

    added: method to find the employee is present or not

--- employee-wage/employee.js
+++ employee-wage/employee.js
@@ -1,1 +1,9 @@
 console.log("Welcome to Employee Program");
+const IS_PRESENT =1;
+let empCheck = Math.floor(Math.random()*10) % 2;
+if (empCheck==IS_PRESENT) {
+    console.log("Employee is Present");
+    return;
+}else{
+    console.log("Employee is absent");
+}
