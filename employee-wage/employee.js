fd8dbfda01e8cb0a37d332d4c7b22b04d8e67d2f
MANOJ-51 <m4anoj51@gmail.com>
2022-07-29 22:37:45 +0530

    added: method to get the details of employee part time or full time

--- employee-wage/employee.js
+++ employee-wage/employee.js
@@ -1,9 +1,22 @@
 console.log("Welcome to Employee Program");
-const IS_PRESENT =1;
-let empCheck = Math.floor(Math.random()*10) % 2;
-if (empCheck==IS_PRESENT) {
-    console.log("Employee is Present");
-    return;
-}else{
-    console.log("Employee is absent");
+
+const FULL_TIME = 1;
+const PART_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let employeeHours = 0;
+empCheck = Math.floor(Math.random()*10) % 3;
+switch(empCheck){
+    case FULL_TIME:
+        employeeHours = FULL_TIME_HOURS;
+        break;
+    case PART_TIME:
+        employeeHours = PART_TIME_HOURS;
+       break;
+    default:
+        employeeHours = 0;
 }
+let employeeWage = employeeHours * WAGE_PER_HOUR;
+console.log("Employee Wage is : "+employeeWage);
