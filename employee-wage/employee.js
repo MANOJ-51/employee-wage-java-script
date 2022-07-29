e8e6e26a2cae85e0f3238cc9a2a59042bb7321b0
MANOJ-51 <m4anoj51@gmail.com>
2022-07-29 22:51:39 +0530

    modified: program using function method

--- employee-wage/employee.js
+++ employee-wage/employee.js
@@ -1,22 +1,23 @@
 console.log("Welcome to Employee Program");
 
+const ABSENT = 0;
 const FULL_TIME = 1;
 const PART_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let employeeHours = 0;
-empCheck = Math.floor(Math.random()*10) % 3;
-switch(empCheck){
-    case FULL_TIME:
-        employeeHours = FULL_TIME_HOURS;
-        break;
-    case PART_TIME:
-        employeeHours = PART_TIME_HOURS;
-       break;
-    default:
-        employeeHours = 0;
+function getEmployeeHours(empCheck) {
+    switch (empCheck) {
+        case FULL_TIME:
+            return FULL_TIME_HOURS;
+        case PART_TIME:
+            return PART_TIME_HOURS;
+        default:
+            return ABSENT;
+    }
 }
-let employeeWage = employeeHours * WAGE_PER_HOUR;
-console.log("Employee Wage is : "+employeeWage);
+let empCheck = Math.floor(Math.random() * 10) % 3;
+
+let employeeWage = getEmployeeHours(empCheck) * WAGE_PER_HOUR;
+console.log("Employee Wage is : " + employeeWage);
